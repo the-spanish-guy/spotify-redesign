4dd221666ca4a2c93fe9863bd0816e1b3a6f705c
Luis Enrique <luyo.lemp@gmail.com>
2020-04-04 20:42:18 -0300

    [API] finished artists, albums and singles

--- src/service/api.js
+++ src/service/api.js
@@ -1,31 +1,216 @@
 {
-  "home": [
+  "artistas": [
     {
-      "nomeAlbum": "Yo Hago Lo que me da la gana",
-      "cover": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fbad-bunny-yhlqmdlg-yo-hago-lo-que-me-da-la-gana-album-stream-1.jpg?q=90&w=1400&cbr=1&fit=max"
-    },
-    {
-      "nomeAlbum": "Yo Hago Lo que me da la gana",
-      "cover": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fbad-bunny-yhlqmdlg-yo-hago-lo-que-me-da-la-gana-album-stream-1.jpg?q=90&w=1400&cbr=1&fit=max"
-    },
-    {
-      "nomeAlbum": "Yo Hago Lo que me da la gana",
-      "cover": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fbad-bunny-yhlqmdlg-yo-hago-lo-que-me-da-la-gana-album-stream-1.jpg?q=90&w=1400&cbr=1&fit=max"
-    },
-    {
-      "nomeAlbum": "Yo Hago Lo que me da la gana",
-      "cover": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fbad-bunny-yhlqmdlg-yo-hago-lo-que-me-da-la-gana-album-stream-1.jpg?q=90&w=1400&cbr=1&fit=max"
-    },
-    {
-      "nomeAlbum": "Yo Hago Lo que me da la gana",
-      "cover": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fbad-bunny-yhlqmdlg-yo-hago-lo-que-me-da-la-gana-album-stream-1.jpg?q=90&w=1400&cbr=1&fit=max"
-    },
-    {
-      "nomeAlbum": "Yo Hago Lo que me da la gana",
-      "cover": "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F02%2Fbad-bunny-yhlqmdlg-yo-hago-lo-que-me-da-la-gana-album-stream-1.jpg?q=90&w=1400&cbr=1&fit=max"
+      "Bad Bunny": {
+        "album": [
+          {
+            "nome_album": "X 100pre",
+            "ano_lancamento": "2018",
+            "cover_album":"https://i.scdn.co/image/ab67616d0000b2732fbd77033247e889cb7d2ac4",
+            "musicas": {
+              "0": "Estamos Bien",
+              "1": "Mia",
+              "2": "Solo de Mí",
+              "3": "Caro",
+              "4": "Si Estuviésemos Juntos",
+              "5": "La Romana",
+              "6": "200 MPH",
+              "7": "Ni Bien Ni Mal"
+            }
+          },
+          {
+            "nome_album": "Oasis",
+            "ano_lancamento": "2019",
+            "cover_album": "https://i.scdn.co/image/ab67616d0000b2734891d9b25d8919448388f3bb",
+            "musicas": {
+              "0": "Mojaita",
+              "1": "Yo Le Llego",
+              "2": "Cuidao por Ahí",
+              "3": "Qué Pretendes",
+              "4": "La Canción",
+              "5": "Un Peso (featuring Marciano Cantero)",
+              "6": "Odio",
+              "7": "Como un Bebé (featuring Mr Eazi)"
+            }
+          },
+          {
+            "nome_album": "YHLQMDLG",
+            "ano_lancamento": "2020",
+            "cover_album": "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+            "musicas": {
+              "0": "Si Veo a Tu Mamá",
+              "1": "La Difícil",
+              "2": "Pero Ya No",
+              "3": "La Santa by Bad Bunny & Daddy Yankee",
+              "4": "Yo Perreo Sola",
+              "5": "Bichiyal by Bad Bunny & Yaviah",
+              "6": "Soliá",
+              "7": "La Zona",
+              "8": "Qué Malo by Bad Bunny & Ñengo Flow",
+              "9": "Vete",
+              "10": "Ignorantes by Bad Bunny & Sech",
+              "11": "A Tu Merced",
+              "12": "Una Vez by Bad Bunny & Mora",
+              "13": "Safaera by Bad Bunny, Jowell & Randy & Ñengo Flow",
+              "14": "25/8",
+              "15": "Está Cabrón Ser Yo by Bad Bunny & Anuel AA",
+              "16": "Puesto Pa' Guerrial by Bad Bunny & Myke Towers",
+              "17": "P FKN R by Bad Bunny, Kendo Kaponi & Arcángel",
+              "18": "Hablamos Mañana by Bad Bunny, Duki & Pablo Chill-E",
+              "19": "˂3"
+            }
+          }
+        ],
+        "single": [
+          {
+            "nome_single": "Soy El Diablo (Remix)",
+            "ano_lancamento": "2019",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273429056794d00e4dced7ac558"
+          },
+          {
+            "nome_single": "Vete",
+            "ano_lancamento": "2019",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273c016a0629154588021d76cd7"
+          },
+          {
+            "nome_single": "Ignorantes",
+            "ano_lancamento": "2020",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273427b8b4d04aef8ed09293b87"
+          },
+          {
+            "nome_single": "LOYAL (feat. Drake and Bad Bunny) [Remix]",
+            "ano_lancamento": "2020",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b2735022fdd919432a678029168c"
+          },
+          {
+            "nome_single": "LOYAL (feat. Drake and Bad Bunny) [Remix]",
+            "ano_lancamento": "2020",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273d591a1338ebe3e0ca58022f5"
+          }
+        ]
+      },
+      "Healy": {
+        "album": [
+          {
+            "nome_album": "Subluxe",
+            "ano_lancamento": "2017",
+            "cover_album": "https://i.scdn.co/image/ab67616d0000b273361bbe0dd5619cc14d81f62a",
+            "musicas": {
+              "0": "Everything (Intro)",
+              "1": "Reckless",
+              "2": "Build",
+              "3": "SLALOM",
+              "4": "Butternut",
+              "5": "No Vacation / Outside",
+              "6": "Dem My Dogs (Ft. Trapo)",
+              "7": "Grape Soda",
+              "8": "Look Like God",
+              "9": "Chaparral",
+              "10": "Python",
+              "11": "Unwind"
+            }
+          }
+        ],
+        "single": [
+          {
+            "nome_single": "$150 / Roll Widdit",
+            "ano_lancamento": "2015",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273447773f14b46614f6f2ea018"
+          },
+          {
+            "nome_single": "Phantoms",
+            "ano_lancamento": "2016",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273249824282d436c61d76f6547"
+          },
+          {
+            "nome_single": "Slalom (feat. PLC)",
+            "ano_lancamento": "2016",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273a5637ec28aecc1284c1f2254"
+          },
+          {
+            "nome_single": "Winse",
+            "ano_lancamento": "2016",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273922dcdf0016b5714d84d8102"
+          },
+          {
+            "nome_single": "Tucson",
+            "ano_lancamento": "2019",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273d8ce26912aed0e55bec81039"
+          },
+          {
+            "nome_single": "Nikes On",
+            "ano_lancamento": "2020",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b273b1d40f40ef2d158ce51b9235"
+          }
+        ]
+      },
+      "Mansionz": {
+        "album": [
+          {
+            "nome_album": "Mansionz",
+            "ano_lancamento": "2017",
+            "cover_album": "https://i.scdn.co/image/ab67616d0000b27390d0e1eb630b1595d23a1a0e",
+            "musicas": {
+              "0": "Snoozefest",
+              "1": "My Beloved",
+              "2": "STFU (Ft. Spark Master Tape)",
+              "3": "Dennis Rodman (Ft. Dennis Rodman)",
+              "4": "i'm thinking about horses",
+              "5": "nobody knows (Ft. Soren Bryce)",
+              "6": "A Million Miles",
+              "7": "Wicked (Ft. G-Eazy)",
+              "8": "Rich White Girls",
+              "9": "Strip Club",
+              "10": "White Linen (Ft. CyHi The Prynce)",
+              "11": "Gorgeous",
+              "12": "The Life of a Troubadour (Ft. Snoozegod)"
+            }
+          }
+        ],
+        "single": [
+          {
+            "nome_single": "Wicked (Rat City Remix)",
+            "ano_lancamento": "2017",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b2735f9131f9ba4a554fb89b745f"
+          },
+          {
+            "nome_single": "Wicked (Loote Remix)",
+            "ano_lancamento": "2017",
+            "cover_single": "https://i.scdn.co/image/ab67616d0000b2735d4c0a0db76d9959bf834672"
+          }
+        ]
+      },
+      "Tove Lo": {
+        "album": [
+          {
+            "nome_alum":"BLUE LIPS (lady wood phase II)",
+            "ano_lancamento": "2017",
+            "cover_album": "https://i.scdn.co/image/ab67616d0000b2735a032c46b63b202e76ebaffe",
+            "musicas": {
+              "0": "LIGHT BEAMS",
+              "1": "disco tits",
+              "2": "shedontknowbutsheknows",
+              "3": "shivering gold",
+              "4": "don't ask don't tell",
+              "5": "stranger",
+              "6": "bitches",
+              "7": "PITCH BLACK",
+              "8": "romantics (Ft. Daye Jack)",
+              "9": "cycles",
+              "10": "struggle",
+              "11": "9th of october",
+              "12": "bad days",
+              "13": "hey you got drugs?"
+            }
+          }
+        ],
+        "single": [
+          {}
+        ]
+      }
     }
   ],
-  "recentes": [
+  "podcast": [
     {
       
     }
