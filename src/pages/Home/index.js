3f859a9a9ab9f9830301a276babdcd3059e9e908
Luis Enrique <luyo.lemp@gmail.com>
2020-04-06 01:18:22 -0300

    [FRONTEND] showing data from the API, I have to refactor it later

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,188 +1,183 @@
-import React from 'react'
-import { View, Text, SafeAreaView, FlatList, ScrollView, Group } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, SafeAreaView, FlatList, ScrollView, Group, Image, ImageBackground } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
+
+import { loadData, loadMusicasMaisOuvidas, loadPlayslistsSugeridas, loadPlayslistsSeguidas, loadPodcastsMaisOuvidas, loadPodcasts } from '../../service/api'
 
 import styles from './styles'
 import { LinearGradient } from 'expo-linear-gradient'
+import Animated from 'react-native-reanimated'
+
+const artistas = loadData()
+const mais_ouvidas = loadMusicasMaisOuvidas()
+const playlist_sugeridas = loadPlayslistsSugeridas()
+const playlist_seguidas = loadPlayslistsSeguidas()
+const podcasts_mais_ouvidas = loadPodcastsMaisOuvidas()
+const podcasts = loadPodcasts()
 
 export default function Home() {
-  return(
-    <SafeAreaView style={styles.container}>
-      <ScrollView
-        scrollEnabled={true}
-      >
-        <LinearGradient colors={['#D1379C', '#9B358B', '#71337E']}
-          style={styles.topBar}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-        >
-          <View style={styles.txtContainer}>
-            <Text style={[styles.txt, { fontSize: 30, paddingBottom: 10 }]}>Suas playlist</Text>
-            <Text style={[styles.txt, { fontSize: 16 }]}>Baila Reggaeton</Text>
-          </View>
-          <View style={styles.txtContainer}>
-            <Ionicons name="ios-settings" size={28} color="#FFFFFF" />
-          </View>
-        </LinearGradient>
 
-        <View style={styles.mpContainer}>
-          <View style={styles.mpTitle}>
-            <Text style={styles.musicas}>Músicas</Text>
-            <Text style={styles.podcasts}>Podcasts</Text>
-          </View>
-          
-          <Text style={
-            {
-              color:"#FFFFFF",
-              paddingBottom: 12,
-              fontSize: 20,
-              marginLeft: 20
-            }
-          }>Lançamentos</Text>
+  const [viewPodcast ,setViewPodcast] = useState(false)
+
+  function viewMusicaOrPodcasts() {
+    if(viewPodcast) {
+      return (
+        <>
+          <Text style={[styles.mpChild, styles.otherTitlesFocused]}>Lançamentos</Text>
           <FlatList
-            data={[1, 2, 3, 4, 5, 6]}
+            data={artistas}
             horizontal
             showsHorizontalScrollIndicator={false}
-            renderItem={() => (
-              <View style={
-                {
-                  width: 140,
-                  height: 170,
-                  borderRadius: 10,
-                  marginHorizontal: 10,
-                  backgroundColor: "#15a3bf",
-                  justifyContent: "flex-end",
-                  paddingBottom: 10,
-                  paddingLeft: 10
-                }
-              }>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20}}>After hours</Text>
-                <Text style={{color:"#ccc", fontWeight: "bold"}}>The Weekend</Text>
-              </View>
-            )}
-          />
-
-          {/* Ativar quando tiver podcasts */}
-          {/* <Text style={
-            {
-              color: "#FFF",
-              fontWeight: "bold",
-              fontSize: 28,
-              marginHorizontal: 20,
-              marginTop: 40,
-              marginBottom: 40,
-            }
-          }>Programas</Text>
-          <FlatList
-            data={[1, 2, 3, 4, 5, 6]}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            renderItem={() => (
-              <View
-                style={{
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
+            renderItem={({ item, index }) => (
+              <ImageBackground
+                source={{
+                  uri: item.ultimos_lancamentos.album.cover_album
                 }}
+                style={styles.releasesBackground}
+                imageStyle={{
+                  borderRadius: 10,
+                }}
+              >
+                <LinearGradient colors={['transparent', 'rgba(18, 19, 21, 0.2)', 'rgba(18, 19, 21, 0.7)', 'rgba(18, 19, 21, 1)']}
+                style={styles.releasesBackgroundGradient}
+                start={{ x: 1, y: 0 }}
+                end={{ x: 1, y: 1 }}
                 >
-                <View style={
-                  {
-                    marginHorizontal: 10,
-                    width: 170,
-                    height: 170,
-                    borderRadius: 100,
-                    backgroundColor: "#15a3bf",
-                    justifyContent: "flex-end",
-                    paddingLeft: 10,
-                  }
-                }>
-                </View>
-                <Text style={
-                  {
-                    marginTop: 10,
-                    color:"#FFF",
-                    fontWeight: "bold",
-                    fontSize: 20,
-
-                  }
-                }>After hours</Text>
-              </View>
-            )}
-          /> */}
-          
-          <View style={{borderLeftWidth: 10, borderLeftColor: "#1db954", width: "100%", marginTop: 80}}>
-            <Text style={{fontWeight: "bold", color:"#FFFFFF", fontSize:26, marginLeft: 8}}>Músicas mais escutadas</Text>
-            <View style={{padding: 20, flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: 80, height: 80, borderRadius: 8, backgroundColor: "#FFF"}}>
-              </View>
-              <View style={{marginLeft: 60}}>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20}}>After hours</Text>
-                <Text style={{color:"#ccc", fontWeight: "bold"}}>The Weekend</Text>
-              </View>
-            </View>
-
-            <View style={{padding: 20, flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: 80, height: 80, borderRadius: 8, backgroundColor: "#FFF"}}>
-              </View>
-              <View style={{marginLeft: 60}}>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20}}>After hours</Text>
-                <Text style={{color:"#ccc", fontWeight: "bold"}}>The Weekend</Text>
-              </View>
-            </View>
-
-            <View style={{padding: 20, flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: 80, height: 80, borderRadius: 8, backgroundColor: "#FFF"}}>
-              </View>
-              <View style={{marginLeft: 60}}>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20}}>After hours</Text>
-                <Text style={{color:"#ccc", fontWeight: "bold"}}>The Weekend</Text>
-              </View>
-            </View>
-
-            <View style={{padding: 20, flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: 80, height: 80, borderRadius: 8, backgroundColor: "#FFF"}}>
-              </View>
-              <View style={{marginLeft: 60}}>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20}}>After hours</Text>
-                <Text style={{color:"#ccc", fontWeight: "bold"}}>The Weekend</Text>
-              </View>
-            </View>
-
-            <View style={{padding: 20, flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: 80, height: 80, borderRadius: 8, backgroundColor: "#FFF"}}>
-              </View>
-              <View style={{marginLeft: 60}}>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20}}>After hours</Text>
-                <Text style={{color:"#ccc", fontWeight: "bold"}}>The Weekend</Text>
-              </View>
-            </View>
-          </View>
-        
-        <Text style={{color: "#FFF", paddingBottom: 40, fontSize: 20, marginLeft: 20, marginTop: 40}}>Playlist para você</Text>
-        <FlatList
-            data={[1, 2, 3, 4, 5, 6]}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            renderItem={() => (
-              <View>
-                <View style={
-                  {
-                    width: 160,
-                    height: 170,
-                    borderRadius: 10,
-                    marginHorizontal: 20,
-                    backgroundColor: "#15a3bf",
-                    paddingLeft: 10,
-                  }
-                }>
-                </View>
-                <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20, marginHorizontal: 20, marginTop: 10 , marginBottom: 30}}>After hours</Text>
-              </View>
+                  <Text style={styles.releasesTitle}>
+                    {
+                      item.ultimos_lancamentos.album.nome_album
+                    }
+                  </Text>
+                  <Text style={styles.releasesSubtitle}>{item.nome_artista}</Text>
+                </LinearGradient>
+              </ImageBackground>
             )}
           />
 
-          {/* Ativar quando tiver podcasts */}
-          {/* <Text style={{
+          <View style={styles.mostListenedToContainer}>
+            <Text style={[styles.subTitleFocused, {marginLeft: 8}]}>Músicas mais escutadas</Text>
+
+            {
+              mais_ouvidas.map(
+                ({ nome_musica, cover_album, nome_artista }) => 
+                  <View style={styles.mostListenedToContainerChild}>
+                    <Image source={{ uri: cover_album }} style={styles.mostListenedToImage}/>
+
+                    <View style={{marginLeft: 20}}>
+                      <Text style={styles.mostListenedToMusic}>{nome_musica}</Text>
+                      <Text style={{color:"#ccc", fontWeight: "bold"}}>{nome_artista}</Text>
+                    </View>
+                  </View>
+              )
+            }
+          </View>
+
+          <Text style={{color: "#FFF", paddingBottom: 40, fontSize: 20, marginLeft: 20, marginTop: 40}}>Playlist para você</Text>
+          <FlatList
+              data={playlist_sugeridas}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <View>
+                  <Image style={
+                    {
+                      width: 160,
+                      height: 170,
+                      borderRadius: 10,
+                      marginHorizontal: 20,
+                      backgroundColor: "#15a3bf",
+                      paddingLeft: 10,
+                    }
+                  }
+                  source={{ uri: item.cover_playlist }}
+                  />
+                  <Text style={{color:"#FFF", fontWeight: "bold", fontSize: 20, marginHorizontal: 20, marginTop: 10 , marginBottom: 30}}>
+                    {item.nome_playlist}
+                  </Text>
+                </View>
+              )}
+            />
+        </>
+      )
+    }
+
+    return (
+      <>
+        <Text style={[styles.mpChild, styles.otherTitlesFocused]}>Podcasts mais escutados</Text>
+        <FlatList
+          data={podcasts_mais_ouvidas}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+            <ImageBackground
+              source={{
+                uri: item.cover_ep
+              }}
+              style={styles.releasesBackground}
+              imageStyle={{
+                borderRadius: 10,
+              }}
+            >
+              <LinearGradient colors={['transparent', 'rgba(18, 19, 21, 0.2)', 'rgba(18, 19, 21, 0.7)', 'rgba(18, 19, 21, 1)']}
+              style={styles.releasesBackgroundGradient}
+              start={{ x: 1, y: 0 }}
+              end={{ x: 1, y: 1 }}
+              >
+                <Text style={styles.releasesSubtitle}>{item.nome_podcast}</Text>
+              </LinearGradient>
+            </ImageBackground>
+          )}
+        />
+
+        <Text style={
+          {
+            color: "#FFF",
+            fontWeight: "bold",
+            fontSize: 28,
+            marginHorizontal: 20,
+            marginTop: 40,
+            marginBottom: 40,
+          }
+        }>Programas</Text>
+        <FlatList
+          data={podcasts}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View
+              style={styles.podcastContainer}
+              >
+                <Image style={styles.podcastImage}
+                source={{ uri: item.cover_podcast}}
+              />
+
+                
+              {/* <View >
+              </View> */}
+              <Text style={styles.podcastName}>{item.nome_podcast}</Text>
+            </View>
+          )}
+        />
+
+        <View style={styles.mostListenedToContainer}>
+          <Text style={[styles.subTitleFocused, {marginLeft: 8}]}>Novos episódios de podcasts</Text>
+
+          {
+            podcasts.map(
+              ({ nome_podcast, ultimo_lancamento }) => 
+                <View style={styles.mostListenedToContainerChild}>
+                  <Image source={{ uri: ultimo_lancamento.cover_ep }} style={styles.mostListenedToImage}/>
+
+                  <View style={{marginLeft: 20}}>
+                    <Text style={styles.mostListenedToMusic}>{ultimo_lancamento.nome_ep}</Text>
+                    <Text style={{color:"#ccc", fontWeight: "bold"}}>{nome_podcast}</Text>
+                  </View>
+                </View>
+            )
+          }
+        </View>
+
+        <Text style={{
             color: "#FFF",
             paddingBottom: 40,
             fontSize: 20,
@@ -190,10 +185,10 @@ export default function Home() {
             marginTop: 40
             }}>Downloads</Text>
           <FlatList
-              data={[1, 2, 3, 4, 5, 6]}
+              data={podcasts}
               horizontal
               showsHorizontalScrollIndicator={false}
-              renderItem={() => (
+              renderItem={({ item }) => (
                 <View
                   style={{
                     width: 240,
@@ -212,15 +207,18 @@ export default function Home() {
                     height: 64,
                   }}
                   >
-                    <View style={
-                      {
-                        width: 60,
-                        height: 60,
-                        borderRadius: 10,
-                        backgroundColor: "#15a3bf",
-                      }
-                    }>
-                    </View>
+                    <Image
+                      style={
+                        {
+                          width: 60,
+                          height: 60,
+                          borderRadius: 10,
+                          backgroundColor: "#15a3bf",
+                        }}
+                        source={{
+                          uri: item.cover_podcast
+                        }}
+                    />
                     <View
                     style={{
                       flexDirection: "column"
@@ -232,12 +230,12 @@ export default function Home() {
                         fontSize: 16,
                         marginHorizontal: 20,
                         marginTop: 10,
-                      }}>Desenhar para desestresar</Text>
+                      }}>Desenhar para deses...</Text>
                       <Text style={{
                         color:"#858585",
                         fontSize: 12,
                         marginHorizontal: 20,
-                        }}>Yellow Cast</Text>
+                        }}>{item.nome_podcast}</Text>
                       </View>
                   </View>
                   <View style={{
@@ -252,7 +250,71 @@ export default function Home() {
 
                 </View>
               )}
-            /> */}
+            />
+
+      </>
+    )
+  }
+
+  return(
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        scrollEnabled={true}
+      >
+        <FlatList
+        style={styles.flatImageContainer}
+        data={playlist_seguidas}
+        keyExtractor={(item, index) => 'key' + index }
+        horizontal
+        scrollEnabled={true}
+        snapToAlignment="center"
+        scrollEventThrottle={16}
+        decelerationRate={"fast"}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+            <ImageBackground
+              source={{ uri: item.cover_playlist }}
+              style={styles.topBarImage}
+              imageStyle={{
+                borderBottomLeftRadius: 20,
+                borderBottomRightRadius: 20
+              }}
+            >
+                <LinearGradient colors={['rgba(18, 19, 21, 0.2)', 'rgba(18, 19, 21, 0.4)', 'rgba(18, 19, 21, 0.8)']}
+                  style={styles.topBar}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 0, y: 1 }}
+                >
+                  <View style={styles.txtContainer}>
+                    <Text style={[styles.subTitleFocused, styles.txt]}>Suas playlist</Text>
+                    <Text style={styles.subTxt}>{item.nome_playlist}</Text>
+                  </View>
+                  <View style={styles.txtContainer}>
+                    <Ionicons name="ios-settings" size={28} color="#FFFFFF" />
+                  </View>
+                </LinearGradient>
+              </ImageBackground>
+          )}
+        />
+
+        <View style={styles.mpContainer}>
+          <View style={styles.mpTitle}>
+            <Text style={ viewPodcast ? styles.subTitleFocused : styles.subTitleDefocused} onPress={() => setViewPodcast(true)}>Músicas</Text>
+            <Text style={[ viewPodcast ? styles.subTitleDefocused : styles.subTitleFocused, styles.podcasts]} onPress={() => setViewPodcast(false)}>Podcasts</Text>
+          </View>
+          
+          {
+            viewMusicaOrPodcasts()
+          }
+          
+            
+          
+          
+        
+          
+
+          {/* Ativar quando tiver podcasts */}
+          
         
         </View>
       </ScrollView>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,24 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import Constants from 'expo-constants'
+
+const { width } = Dimensions.get('screen')
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121315",
   },
+  
+  flatImageContainer: {
+    flex: 1,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10
+  },
+  topBarImage: {
+    width: width,
+    height: 300,
+  },
   topBar: {
-    width: '100%',
     height: 300,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
@@ -19,11 +30,16 @@ export default StyleSheet.create({
     paddingHorizontal: 20
   },
   txt: {
-    fontWeight: "bold",
+    fontSize: 30,
+    paddingBottom: 10
+  },
+  subTxt: {
+    fontSize: 16,
     color: "#FFFFFF"
   },
+  
   mpContainer: {
-    marginTop: 20,
+    marginTop: 35,
   },
   mpTitle: {
     flexDirection: "row",
@@ -36,8 +52,104 @@ export default StyleSheet.create({
     fontSize: 18
   },
   podcasts: {
-    color: "#e3e3e3",
+    // color: "#e3e3e3",
     marginHorizontal: 20,
-    fontSize: 18
+  },
+  mpChild: {
+    marginLeft: 20,
+    paddingBottom: 16,
+  },
+
+  releasesBackground:{
+    width: 140,
+    height: 170,
+    marginLeft: 26,
+    marginRight: 6,
+    // marginHorizontal: 10,
+  },
+  releasesBackgroundGradient: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+    paddingBottom: 10,
+    paddingLeft: 10
+  },
+  releasesTitle:{
+    color:"#FFF",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  releasesSubtitle: {
+    color:"#ccc", fontWeight: "bold"
+
+  },
+
+  podcastContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  podcastImage:{
+    marginHorizontal: 10,
+    width: 170,
+    height: 170,
+    borderRadius: 100,
+    justifyContent: "flex-end",
+    paddingLeft: 10,
+  },
+  podcastName: 
+  {
+    marginTop: 10,
+    color:"#FFF",
+    fontWeight: "bold",
+    fontSize: 20,
+
+  },
+  mostListenedToContainer:{
+    borderLeftWidth: 10,
+    borderLeftColor: "#1db954",
+    marginTop: 80
+  },
+  mostListenedToContainerChild:{
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  mostListenedToImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+    backgroundColor: "#FFF"
+  },
+  mostListenedToMusic: {
+    color:"#FFF",
+    fontWeight: "bold",
+    fontSize: 20
+  },
+
+  titleFocused: {
+    color: "#FFFFFF",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subTitleFocused: {
+    color: "#FFFFFF",
+    fontSize: 28,
+    fontWeight: "bold"
+  },
+  titleDefocused: {
+    color: "#ACACAD",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  subTitleDefocused: {
+    color: "#ACACAD",
+    fontSize: 28,
+    fontWeight: "bold"
+  },
+  otherTitlesFocused: {
+    color: "#FFFFFF",
+    fontSize: 26,
+    fontWeight: "bold"
   }
 })
