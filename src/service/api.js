97a1bf25575639e456adc1a3aad8febc61ee9f25
Luis Enrique <luyo.lemp@gmail.com>
2020-04-12 15:47:29 -0300

    [REFACTOR] refactor API

--- src/service/api.js
+++ src/service/api.js
@@ -8,58 +8,235 @@ function loadData() {
           "nome_album": "X 100pre",
           "ano_lancamento": "2018",
           "cover_album":"https://i.scdn.co/image/ab67616d0000b2732fbd77033247e889cb7d2ac4",
-          "musicas": {
-            "0": "Estamos Bien",
-            "1": "Mia",
-            "2": "Solo de Mí",
-            "3": "Caro",
-            "4": "Si Estuviésemos Juntos",
-            "5": "La Romana",
-            "6": "200 MPH",
-            "7": "Ni Bien Ni Mal"
-          }
+          "musicas": [
+            {
+              "id": 1,
+              "nome": "NI BIEN NI MAL",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 2,
+              "nome": "200 MPH",
+              "feat": "Bad Bunny, Diplo"
+            },
+            {
+              "id": 3,
+              "nome": "¿Quién Tú Eres? "
+            },
+            {
+              "id": 4,
+              "nome": "Caro ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 5,
+              "nome": "Tenemos Que Hablar ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 6,
+              "nome": "Otra Noche en Miami ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 7,
+              "nome": "Ser Bichote ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 8,
+              "nome": "Si Estuviésemos Juntos ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 9,
+              "nome": "Solo de Mí ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 10,
+              "nome": "Cuando Perriabas ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 11,
+              "nome": "La Romana ",
+              "feat": "Bad Bunny, El Alfa"
+            },
+            {
+              "id": 12,
+              "nome": "Como Antes ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 13,
+              "nome": "RLNDT ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 14,
+              "nome": "Estamos Bien ",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 15,
+              "nome": "MÍA ",
+              "feat": "Bad Bunny, Drake"
+            }
+          ]
         },
         {
           "nome_album": "Oasis",
           "ano_lancamento": "2019",
           "cover_album": "https://i.scdn.co/image/ab67616d0000b2734891d9b25d8919448388f3bb",
-          "musicas": {
-            "0": "Mojaita",
-            "1": "Yo Le Llego",
-            "2": "Cuidao por Ahí",
-            "3": "Qué Pretendes",
-            "4": "La Canción",
-            "5": "Un Peso (featuring Marciano Cantero)",
-            "6": "Odio",
-            "7": "Como un Bebé (featuring Mr Eazi)"
-          }
+          "musicas": [
+            {
+              "id": 1,
+              "nome": "Mojaita",
+            },
+            {
+              "id": 2,
+              "nome":"Yo Le Llego",
+              "feat": "J Balvin, Bad Bunny"
+            },
+            {
+              "id": 3,
+              "nome":"Cuidao por Ahí",
+              "feat": "J Balvin, Bad Bunny"
+            },
+            {
+              "id": 4,
+              "nome":"Qué Pretendes",
+              "feat": "J Balvin, Bad Bunny"
+            },
+            {
+              "id": 5,
+              "nome":"La Canción",
+              "feat": "J Balvin, Bad Bunny"
+            },
+            {
+              "id": 6,
+              "nome":"Un Peso",
+              "feat": "J Balvin, Bad Bunny, Marciano Cantero"
+            },
+            {
+              "id": 7,
+              "nome":"Odio",
+              "feat": "J Balvin, Bad Bunny"
+            },
+            {
+              "id": 8,
+              "nome":"Como un Bebé",
+              "feat": "J Balvin, Bad Bunny, Mr Eazi"
+            },
+          ]
         },
         {
           "nome_album": "YHLQMDLG",
           "ano_lancamento": "2020",
           "cover_album": "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
-          "musicas": {
-            "0": "Si Veo a Tu Mamá",
-            "1": "La Difícil",
-            "2": "Pero Ya No",
-            "3": "La Santa by Bad Bunny & Daddy Yankee",
-            "4": "Yo Perreo Sola",
-            "5": "Bichiyal by Bad Bunny & Yaviah",
-            "6": "Soliá",
-            "7": "La Zona",
-            "8": "Qué Malo by Bad Bunny & Ñengo Flow",
-            "9": "Vete",
-            "10": "Ignorantes by Bad Bunny & Sech",
-            "11": "A Tu Merced",
-            "12": "Una Vez by Bad Bunny & Mora",
-            "13": "Safaera by Bad Bunny, Jowell & Randy & Ñengo Flow",
-            "14": "25/8",
-            "15": "Está Cabrón Ser Yo by Bad Bunny & Anuel AA",
-            "16": "Puesto Pa' Guerrial by Bad Bunny & Myke Towers",
-            "17": "P FKN R by Bad Bunny, Kendo Kaponi & Arcángel",
-            "18": "Hablamos Mañana by Bad Bunny, Duki & Pablo Chill-E",
-            "19": "˂3"
-          }
+          "musicas": [
+            {
+              "id": 1,
+              "nome": "Si Veo a Tu Mamá",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 2,
+              "nome": "La Difícil",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 3,
+              "nome": "Pero Ya No",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 4,
+              "nome": "La Santa",
+              "feat": "Bad Bunny, Daddy Yankee"
+            },
+            {
+              "id": 5,
+              "nome": "Yo Perreo Sola",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 6,
+              "nome": "Bichiyal",
+              "feat": "Bad Bunny, Yaviah"
+            },
+            {
+              "id": 7,
+              "nome": "Soliá",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 8,
+              "nome": "La Zona",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 9,
+              "nome": "Qué Malo",
+              "feat": "Bad Bunny, Ñengo Flow"
+            },
+            {
+              "id": 10,
+              "nome": "Vete",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 11,
+              "nome": "Ignorantes",
+              "feat": "Bad Bunny, Sech"
+            },
+            {
+              "id": 12,
+              "nome": "A Tu Merced",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 13,
+              "nome": "Una Vez",
+              "feat": "Bad Bunny, Mora"
+            },
+            {
+              "id": 14,
+              "nome": "Safaera",
+              "feat": "Bad Bunny, Jowell & Randy, Ñengo Flow"
+            },
+            {
+              "id": 15,
+              "nome": "25/8",
+              "feat": "Bad Bunny"
+            },
+            {
+              "id": 16,
+              "nome": "Está Cabrón Ser Yo",
+              "feat": "Bad Bunny, Anuel AA"
+            },
+            {
+              "id": 17,
+              "nome": "Puesto Pa' Guerrial",
+              "feat": "Bad Bunny, Myke Towers"
+            },
+            {
+              "id": 18,
+              "nome": "P FKN R",
+              "feat": "Bad Bunny, Kendo Kaponi & Arcángel"
+            },
+            {
+              "id": 19,
+              "nome": "Hablamos Mañana",
+              "feat": "Bad Bunny, Duki & Pablo Chill-E"
+            },
+            {
+              "id": 20,
+              "nome": "˂3",
+              "feat": "Bad Bunny"
+            },
+          ]
         }
       ],
       "single": [
@@ -132,20 +309,68 @@ function loadData() {
           "nome_album": "Subluxe",
           "ano_lancamento": "2017",
           "cover_album": "https://i.scdn.co/image/ab67616d0000b273361bbe0dd5619cc14d81f62a",
-          "musicas": {
-            "0": "Everything (Intro)",
-            "1": "Reckless",
-            "2": "Build",
-            "3": "SLALOM",
-            "4": "Butternut",
-            "5": "No Vacation / Outside",
-            "6": "Dem My Dogs (Ft. Trapo)",
-            "7": "Grape Soda",
-            "8": "Look Like God",
-            "9": "Chaparral",
-            "10": "Python",
-            "11": "Unwind"
-          }
+          "musicas": [
+            {
+              "id": 1,
+              "nome": "Everything (Intro)",
+              "feat": "Healy"
+            },
+            {
+              "id": 2,
+              "nome": "Reckless",
+              "feat": "Healy"
+            },
+            {
+              "id": 3,
+              "nome": "Build",
+              "feat": "Healy"
+            },
+            {
+              "id": 4,
+              "nome": "SLALOM",
+              "feat": "Healy"
+            },
+            {
+              "id": 5,
+              "nome": "Butternut",
+              "feat": "Healy"
+            },
+            {
+              "id": 6,
+              "nome": "No Vacation / Outside",
+              "feat": "Healy"
+            },
+            {
+              "id": 7,
+              "nome": "Dem My Dogs",
+              "feat": "Healy, Trapo"
+            },
+            {
+              "id": 8,
+              "nome": "Grape Soda",
+              "feat": "Healy"
+            },
+            {
+              "id": 9,
+              "nome": "Look Like God",
+              "feat": "Healy"
+            },
+            {
+              "id": 10,
+              "nome": "Chaparral",
+              "feat": "Healy"
+            },
+            {
+              "id": 11,
+              "nome": "Python",
+              "feat": "Healy"
+            },
+            {
+              "id": 12,
+              "nome": "Unwind",
+              "feat": "Healy"
+            },
+          ]
         }
       ],
       "single": [
@@ -215,21 +440,73 @@ function loadData() {
           "nome_album": "Mansionz",
           "ano_lancamento": "2017",
           "cover_album": "https://i.scdn.co/image/ab67616d0000b27390d0e1eb630b1595d23a1a0e",
-          "musicas": {
-            "0": "Snoozefest",
-            "1": "My Beloved",
-            "2": "STFU (Ft. Spark Master Tape)",
-            "3": "Dennis Rodman (Ft. Dennis Rodman)",
-            "4": "i'm thinking about horses",
-            "5": "nobody knows (Ft. Soren Bryce)",
-            "6": "A Million Miles",
-            "7": "Wicked (Ft. G-Eazy)",
-            "8": "Rich White Girls",
-            "9": "Strip Club",
-            "10": "White Linen (Ft. CyHi The Prynce)",
-            "11": "Gorgeous",
-            "12": "The Life of a Troubadour (Ft. Snoozegod)"
-          }
+          "musicas": [
+            {
+              "id": 1,
+              "nome": "Snoozefest",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 2,
+              "nome": "My Beloved",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 3,
+              "nome": "STFU",
+              "feat": "Mansionz, Spark Master Tape"
+            },
+            {
+              "id": 4,
+              "nome": "Dennis Rodman",
+              "feat": "Mansionz, Dennis Rodman"
+            },
+            {
+              "id": 5,
+              "nome": "i'm thinking about horses",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 6,
+              "nome": "nobody knows",
+              "feat": "Mansionz, Soren Bryce"
+            },
+            {
+              "id": 7,
+              "nome": "A Million Miles",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 8,
+              "nome": "Wicked",
+              "feat": "Mansionz, G-Eazy"
+            },
+            {
+              "id": 9,
+              "nome": "Rich White Girls",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 10,
+              "nome": "Strip Club",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 11,
+              "nome": "White Linen",
+              "feat": "Mansionz, CyHi The Prynce"
+            },
+            {
+              "id": 12,
+              "nome": "Gorgeous",
+              "feat": "Mansionz"
+            },
+            {
+              "id": 13,
+              "nome": "The Life of a Troubadour",
+              "feat": "Mansionz, Snoozegod"
+            },
+          ]
         }
       ],
       "single": [
@@ -280,22 +557,78 @@ function loadData() {
           "nome_album":"BLUE LIPS (lady wood phase II)",
           "ano_lancamento": "2017",
           "cover_album": "https://i.scdn.co/image/ab67616d0000b2735a032c46b63b202e76ebaffe",
-          "musicas": {
-            "0": "LIGHT BEAMS",
-            "1": "disco tits",
-            "2": "shedontknowbutsheknows",
-            "3": "shivering gold",
-            "4": "don't ask don't tell",
-            "5": "stranger",
-            "6": "bitches",
-            "7": "PITCH BLACK",
-            "8": "romantics (Ft. Daye Jack)",
-            "9": "cycles",
-            "10": "struggle",
-            "11": "9th of october",
-            "12": "bad days",
-            "13": "hey you got drugs?"
-          }
+          "musicas": [
+            {
+              "id": 1,
+              "nome": "LIGHT BEAMS",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 2,
+              "nome": "disco tits",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 3,
+              "nome": "shedontknowbutsheknows",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 4,
+              "nome": "shivering gold",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 5,
+              "nome": "don't ask don't tell",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 6,
+              "nome": "stranger",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 7,
+              "nome": "bitches",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 8,
+              "nome": "PITCH BLACK",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 9,
+              "nome": "romantics (Ft. Daye Jack)",
+              "feat": "Tove Lo, Daye Jack"
+            },
+            {
+              "id": 10,
+              "nome": "cycles",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 11,
+              "nome": "struggle",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 12,
+              "nome": "9th of october",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 13,
+              "nome": "bad days",
+              "feat": "Tove Lo"
+            },
+            {
+              "id": 14,
+              "nome": "hey you got drugs?",
+              "feat": "Tove Lo"
+            }    
+          ]
         }
       ],
       "single": [
